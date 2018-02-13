f443b9e96cd6efd949b9dc788b2ebe77486a6d53
Ivan <ivan.scdo@gmail.com>
2018-02-12 22:44:33 -0600

    pc m212.22:40 js draft1.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,47 @@
-
-
 document.addEventListener("keyup", function( event ) {
-    var wordList = ["html", "css", "javascript"];
-    var gameboard = []; 
-    var startMsg = document.querySelector(".start-msg")
-    startMsg.innerHTML = "Let's begin!"
+var wordList = ["html", "css", "javascript"];
+var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
+var userGuess = event.key;
 
-    var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
+if (computerChoice.split("").indexOf(userGuess) !== -1 ) {
+    console.log("user guess matched letter in word!" )
+}
+
+});
+
+
+
+// document.addEventListener("keyup", function( event ) {
+//     var wordList = ["html", "css", "javascript"];
+//     var gameboard = []; 
+//     var startMsg = document.querySelector(".start-msg")
+//     startMsg.innerHTML = "Let's begin!"
+
+//     var userGuess = event.key;
+//     console.log(userGuess);
+//     var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
     
-    // console.log(computerChoice, "-",  computerChoice.length)
+//     // console.log(computerChoice, "-",  computerChoice.length)
 
-    var gameboardLength = computerChoice.length
+//     console.log(computerChoice.split("").indexOf(x));
+
+//     var gameboardLength = computerChoice.length
        
-    for (i = 0; i < gameboardLength; i++){
-        gameboard.unshift("_");
+//     for (i = 0; i < gameboardLength; i++){
+//         gameboard.unshift("_");
         
-        var unshiftGameboard = document.querySelector(".gameboard")
+//         var unshiftGameboard = document.querySelector(".gameboard")
  
-        unshiftGameboard.innerHTML = gameboard
-    }
-    gameboard = [];
+//         unshiftGameboard.innerHTML = gameboard
+//     }
+//     gameboard = [];
 
 
-});
+// });
 
 
-document.addEventListener("keyup", function( event ) {
+// document.addEventListener("keyup", function( event ) {
 
     
-    console.log(computerChoice)
-});
+//     console.log(computerChoice)
+// });
