8e4844af5b23398640c8437e4719d813e0d7c465
Ivan Saucedo <ivan.scdo@gmail.com>
2018-02-16 21:04:46 -0600

    mb f216.23:02 .js v3.0 - solved current word issue, userGuesses sticking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,19 +7,13 @@ document.querySelector(".start-button").addEventListener("click", function(){
     
     // variables
 
-    // counter for gameboard builds
-    var counter = 0;
-
-    // guess remaining div
+    // guesses remaining div
     var guessesRemaining = "15";
     document.querySelector(".guesses-remaining").innerHTML = guessesRemaining;
 
     // letters guessed div
     var lettersGuessed = "";
     document.querySelector(".letters-guessed").innerHTML = lettersGuessed;
-
-    //stores list of correct guesses
-    var gameboardIndexList = [];
 
     // list of words for computer to pick
     var wordList = ["html", "css", "javascript"];
@@ -31,7 +25,7 @@ document.querySelector(".start-button").addEventListener("click", function(){
     // take length from word computer picked and stored into gameboardLength(int)
     var gameboardLength = computerChoice.length
 
-    // empty variable used to create underscores
+    // empty array used to create underscores in p.gameboard
     var gameboard = []; 
 
     // start message
@@ -40,114 +34,52 @@ document.querySelector(".start-button").addEventListener("click", function(){
     // for loop creates underscores in Current Word div based on word length
     for (var i = 0; i < gameboardLength; i++){
         // adds underscores to var gameboard based on word length
-        gameboard += "_ ";
-        // takes var gameboard and inserts to div p.gameboard
-        document.querySelector(".gameboard").innerHTML = gameboard;
+        gameboard.push(" _ ");
+        // takes var gameboard and pushes to p.gameboard
+        document.querySelector(".gameboard").innerHTML = gameboard.join('');
     }
-    console.log("gameboard: ", gameboard);
-    // console.log("gameboard[0]: ", gameboard[0]);
-    // console.log("computerChoice[0]: ", computerChoice[0]);
+    console.log("gameboard:", gameboard);
+    // console.log("gameboard[0]:", gameboard[0]);
+    // console.log("computerChoice[0]:", computerChoice[0]);
 
     // listens for key presses
     document.addEventListener("keyup", function( event ) {
 
-        // key presses stored into var userGuess
+        // key presses stored into userGuess
         var userGuess = event.key;  
 
-        //.split - splits randomly chosen word (computerChoice) into letters, then inserts into array 
-        //.indexOf -  checks if keypresses (userGuess) match letter in array
+        // .split - splits randomly chosen word (computerChoice) into letters, then inserts into array 
+        // .indexOf -  checks if keypresses (userGuess) match letter in array
+        // === -1 means there is no match, !=== means there is a match
         if (computerChoice.split("").indexOf(userGuess) !== -1 ) {
             
             // if userGuess matches a letter in computerChoice, find the index of that letter in computerChoice and store into gameboardIndex(int)
             var gameboardIndex = computerChoice.indexOf(userGuess);
-            console.log("gameboardIndex: ", gameboardIndex);
-
-            // fromIndex used as starting point for nested if, if2
-            var fromIndex = gameboardIndex + 1;
-
-            // if2 checks if there are any repeating characters in letter, starts search at fromIndex, the next index over from the first matching guess; stores value in gameboardFromIndex (int)
-            if (computerChoice.split("").indexOf(userGuess, fromIndex) !== -1 ) {
-                var gameboardFromIndex = computerChoice.indexOf(userGuess, fromIndex);
-                console.log("gameboardIndex: ", gameboardIndex)
-                console.log("gameboardFromIndex: ", gameboardFromIndex)
-            }
-
-            // takes index of correct guess and stores into array, used for reference in gameboard rebuild
-            // in progress...
-            // for (var i = 0; i < gameboardIndexList.length; i++) {
-
-
-            // }
-            // gameboardIndexList.push(gameboardIndex);
-            // console.log("gameboardIndexList: ", gameboardIndexList);
-            
-
-
-            // example: gameboard/userGuess
-            // var blanks = ["_", "_", "_", "_", "_"];
-            // function keyHandler(){
-            //     var word = "blank";
-            //     var userGuess = "a";
-            //     for( var i = 0; i < word.length; i++ ){
-            //         if( word[i] === userGuess ){
-            //             blanks[i] = userGuess;
-            //         }
-            //     }
-            // }
-
-
-            // if (counter === 0) {
-            //     gameboard = "";
-            // }
-
-            // gameboardBuilding v1.0
-            // gameboard = "";
-            // // for loop - gameboard rebuild
-            // // adds functionality to add userGuess to gameboard
-            // function gameboardBuilder () {
-            //     for (var i = 0; i < gameboardLength; i++){
-            //         if (i === gameboardIndex) {
-            //             gameboard += userGuess + " ";
-            //         } else {
-            //             // adds underscores to var gameboard based on word length
-            //             gameboard += "_ ";
-            //             // takes var gameboard and inserts to div p.gameboard
-            //         }
-            //         counter++;
-                    
-            //     }
-            //     // takes rebuilt gameboard and stores into p.gameboard div
-            //     document.querySelector(".gameboard").innerHTML = gameboard;
-            //     console.log(gameboard);
-            // }
-            // gameboardBuilder();
-
-            // gameboardBuilding v2.0
-            // gameboard = "";
-            // for loop - gameboard rebuild
-            // adds functionality to add userGuess to gameboard
+            console.log("gameboardIndex: ", gameboardIndex);          
+            // builds gameboard with userGuesses
+            // for loop iterates over length of computerChoice (gameboardLength), checks each index of computerChoice, if userGuess is equal to the index, userGuess is set equal to that index in gameboard
             function gameboardBuilder () {
                 for (var i = 0; i < gameboardLength; i++){
                     if (computerChoice[i] === userGuess) {
                         var checker = userGuess;
                         gameboard[i] = userGuess;
-                        console.log("---infunc/inif---");
-                        console.log("userGuess:", userGuess);
-                        console.log("computerChoice[i]:", computerChoice[i]);
-                        console.log("gameboard[i]:", gameboard[i]);
-                        console.log("checker:", checker);
-                       
+                        // console.log("---infunc/inif---");
+                        // console.log("userGuess:", userGuess);
+                        // console.log("computerChoice[i]:", computerChoice[i]);
+                        // console.log("gameboard[i]:", gameboard[i]);
+                        // console.log("gameboard:", gameboard);
+                        // console.log("checker:", checker);
                     } 
-                    
                 }
+
                 // takes rebuilt gameboard and stores into p.gameboard div
-                document.querySelector(".gameboard").innerHTML = gameboard;
+                // .join(''); joins items in array into single string, eliminates commas in between underscores/letters
+                document.querySelector(".gameboard").innerHTML = gameboard.join('');
                 // console.log("inside func, gameboard: ", gameboard);
                 // console.log("inside func, userGuess: ", userGuess);
                 // console.log("inside func, gameboard[0]: ", gameboard[0]);
                 // console.log("inside func, computerChoice[0]: ", computerChoice[0]);
                 // console.log("inside func, computerChoice: ", computerChoice);
-                
             }
             gameboardBuilder();
 
